bf82f69f0f459c49f4ecc759a84a8a6fa76aff43
Ricky Clegg <me@rickyclegg.com>
2018-09-17 21:06:09 +0100

    Hard code first test to get started

--- app/scripts/VenueMapper.spec.js
+++ app/scripts/VenueMapper.spec.js
@@ -15,4 +15,18 @@ describe('VenueMapper', () => {
 
     expect(vm.getPlacesToGo()).toEqual(['El Cantina']);
   });
+
+  it('should return no venues when there are no matches', () => {
+    const user = {
+      name: 'John Davis',
+      wont_eat: ['Mexican']
+    };
+    const venue = {
+      food: ['Mexican'],
+      name: 'El Cantina'
+    };
+    const vm = new VenueMapper([user], [venue]);
+
+    expect(vm.getPlacesToGo()).toEqual([]);
+  });
 });
