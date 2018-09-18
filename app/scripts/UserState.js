00046675ef865658c47240d2d35c7277389fbb17
Ricky Clegg <me@rickyclegg.com>
2018-09-18 09:49:20 +0100

    Add and remove users from state

--- app/scripts/UserState.js
+++ app/scripts/UserState.js
@@ -8,8 +8,24 @@ export default class UserState {
   }
 
   add(user) {
-    if (!this._selectedUsers.some(selUser => selUser.name === user.name)) {
-      this._selectedUsers = [...this._selectedUsers, user];
+    if (!this.selectedUsers.some(selUser => selUser.name === user.name)) {
+      this._selectedUsers = [...this.selectedUsers, user];
+    }
+  }
+
+  remove(user) {
+    const userName = typeof user === 'object' ? user.name : user;
+    let userIndex = -1;
+
+    this.selectedUsers.some((selUser, index) => {
+      userIndex = selUser.name === userName && index || -1;
+
+      return userIndex > -1;
+    });
+
+    if (userIndex > -1) {
+      this._selectedUsers = [...this.selectedUsers];
+      this._selectedUsers.splice(userIndex, 1);
     }
   }
 }
--- app/scripts/UserState.spec.js
+++ app/scripts/UserState.spec.js
@@ -1,13 +1,16 @@
 /* eslint-disable camelcase */
 import UserState from './UserState';
 
+// User old school require to easily get test data
+const usersData = require('../data/users.json');
+
 describe('UserState', () => {
   describe('Adding users', () => {
     it('should return one extra user when adding a NEW user', () => {
       const users = new UserState();
       const currentUsersTotal = users.selectedUsers.length;
 
-      users.add({name: 'John Davis'});
+      users.add(usersData[0]);
 
       expect(users.selectedUsers.length).toEqual(currentUsersTotal + 1);
     });
@@ -16,7 +19,7 @@ describe('UserState', () => {
       const users = new UserState();
       const currentUsers = users.selectedUsers;
 
-      users.add({name: 'John Davis'});
+      users.add(usersData[0]);
 
       expect(users.selectedUsers).not.toBe(currentUsers);
     });
@@ -25,10 +28,61 @@ describe('UserState', () => {
       const users = new UserState();
       const currentUsersTotal = users.selectedUsers.length;
 
-      users.add({name: 'John Davis'});
-      users.add({name: 'John Davis'});
+      users.add(usersData[0]);
+      users.add(usersData[0]);
 
       expect(users.selectedUsers.length).toEqual(currentUsersTotal + 1);
+    });
+  });
+
+  describe('Removing users', () => {
+    it('should return one less users when removing a user', () => {
+      const users = new UserState();
+      let currentUsersTotal = 0;
+
+      users.add(usersData[0]);
+      users.add(usersData[1]);
+      users.add(usersData[2]);
+
+      currentUsersTotal = users.selectedUsers.length;
+
+      users.remove(usersData[1]);
+
+      expect(users.selectedUsers.length).toEqual(currentUsersTotal - 1);
+    });
+
+    it('should return one less users when removing a user by name', () => {
+      const users = new UserState();
+      let currentUsersTotal = 0;
+
+      users.add(usersData[0]);
+      users.add(usersData[1]);
+      users.add(usersData[2]);
+
+      currentUsersTotal = users.selectedUsers.length;
+
+      users.remove('Gary Jones');
+
+      expect(users.selectedUsers[0].name).toEqual(usersData[0].name);
+      expect(users.selectedUsers[1].name).toEqual(usersData[2].name);
+    });
+
+    it('should return the same number of user when removing the same user twice', () => {
+      const users = new UserState();
+      let currentUsersTotal = 0;
+
+      users.add(usersData[0]);
+      users.add(usersData[1]);
+      users.add(usersData[2]);
+
+      currentUsersTotal = users.selectedUsers.length;
+
+      users.remove('Gary Jones');
+      users.remove('Gary Jones');
+
+      expect(users.selectedUsers.length).toEqual(2);
+      expect(users.selectedUsers[0].name).toEqual(usersData[0].name);
+      expect(users.selectedUsers[1].name).toEqual(usersData[2].name);
     });
   });
 });
